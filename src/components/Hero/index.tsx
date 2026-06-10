3b3076eb3c753372f556448e451bd285ccbb0f87
GilsonGarciaJunior <juniorrodrigues082006@gmail.com>
2026-06-10 18:32:43 -0300

    first commit

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,10 @@
+
+const Hero = () => {
+    return (
+        <div className="container">
+            <h1>Viva experiências gastronômicas no conforto da sua casa</h1>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalCss= createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style-type: none;
+    text-decoration: none;
+  }
+
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto
+  }
+`
